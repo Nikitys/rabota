0b9c4176f1bda722108750ef332a169387f4a4df
Никита Толкач <nikitatolkac@MacBook-Pro-Nikita.local>
2017-07-14 17:15:45 +0300

    Start commit

---
+++ app/js/main.js
@@ -0,0 +1,14 @@
+// $('.services-list').owlCarousel({
+//     loop:true,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:3
+//         },
+//         1000:{
+//             items:5
+//         }
+//     }
+// })
